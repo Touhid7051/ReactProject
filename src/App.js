4e689da3afc547561143a44166012d50f43b3396
Touhid <touhidul15-7051@diu.edu.bd>
2020-12-14 17:19:51 +0600

    Js function as html Element in jsx

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import './App.css';
 import React,{Component, createElement} from 'react';
+import Person from './Person/Person' ;
 
 class App extends Component{
 
 
   render(){
-   // return(
-   //   <div className ="App" >
-   //     <h1>HI</h1>
-   //   </div>
-   // );
-   return React.createElement('div' , {className :"App"} ,React.createElement('h1' , null ,'hello' ));
+    return(
+      <div className ="App" >
+        <h1>HI</h1>
+        <p>This is working</p>
+        <Person />
+      </div>
+    );
+  // return React.createElement('div' , {className :"App"} ,React.createElement('h1' , null ,'hello' ));
   }
 }
 
