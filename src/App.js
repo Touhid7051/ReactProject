a9e824031dd3b6a242c44b3453a6d64547a508b8
Touhid <touhidul15-7051@diu.edu.bd>
2020-12-15 15:34:37 +0600

    State Creation

--- src/App.js
+++ src/App.js
@@ -3,16 +3,23 @@ import React,{Component, createElement} from 'react';
 import Person from './Person/Person' ;
 
 class App extends Component{
-
+  state ={
+    persons :[
+      {name:'tou' , age: 27},
+      {name:'Rafi' , age: 27},
+      {name:'Rajeeb' , age: 27},
+    ]
+  }
 
   render(){
     return(
       <div className ="App" >
         <h1>HI</h1>
         <p>This is working</p>
-        <Person name="Touhid" age="17"/>
-        <Person name="Rafi" age="27">Hobbies: Racing </Person>
-        <Person name="Rajeeb" age="37"/>
+        <button>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>Hobbies: Racing </Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
   // return React.createElement('div' , {className :"App"} ,React.createElement('h1' , null ,'hello' ));
