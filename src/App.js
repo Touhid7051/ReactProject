ff2167266b969e77d63e7ba7f4a89daa5f42c051
Touhid <touhidul15-7051@diu.edu.bd>
2020-12-15 15:58:20 +0600

    State Handling and Manupulating DOM

--- src/App.js
+++ src/App.js
@@ -8,7 +8,21 @@ class App extends Component{
       {name:'tou' , age: 27},
       {name:'Rafi' , age: 27},
       {name:'Rajeeb' , age: 27},
-    ]
+    ],
+    otherState: 'some value'
+  }
+
+  switchNameHandler =() => {
+    //console.log('Clicked');
+    //DON'Do THIS: this.state.persons[0].name = 'Touhid';
+    this.setState({
+      persons :[
+        {name:'touhid' , age: 27},
+        {name:'Rappee' , age: 29},
+        {name:'Rajeebaa' , age: 23},
+      ],
+
+    })
   }
 
   render(){
@@ -16,7 +30,7 @@ class App extends Component{
       <div className ="App" >
         <h1>HI</h1>
         <p>This is working</p>
-        <button>Switch Name</button>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>Hobbies: Racing </Person>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
