b32ea92c364b4a0404f61f87c2304aa198f4e81a
Touhid <touhidul15-7051@diu.edu.bd>
2021-01-02 18:21:31 +0600

    use of this.props

--- src/containers/App.js
+++ src/containers/App.js
@@ -75,6 +75,7 @@ togglePersonsHandler = () =>{
       
       <div className ={classes.App} >
         <Cockpit
+          title={this.props.appTitle}
           showPersons={this.state.showPersons}
           persons={this.state.persons}
           clicked={this.togglePersonsHandler} />
