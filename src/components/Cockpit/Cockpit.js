0eb44100590be3d91d580c1d723e3a944f51a868
Touhid <touhidul15-7051@diu.edu.bd>
2021-01-14 15:05:07 +0600

    optimizing funtional components with React.memo()

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -29,11 +29,11 @@ const Cockpit = (props) => {
               btnClass=classes.Red;
         }
           let assignedClasses = [];
-              if(props.persons.length <=2){
+              if(props.personsLength <=2){
                 assignedClasses.push(classes.red); //classes =['red']
               }
 
-              if(props.persons.length <=1){
+              if(props.personsLength <=1){
                 assignedClasses.push(classes.bold); //classes= ['red',bold]
               }
 
@@ -49,4 +49,4 @@ const Cockpit = (props) => {
           );
 }
 
-export default Cockpit;
+export default React.memo(Cockpit);
--- src/containers/App.js
+++ src/containers/App.js
@@ -105,7 +105,7 @@ togglePersonsHandler = () =>{
         <Cockpit
           title={this.props.appTitle}
           showPersons={this.state.showPersons}
-          persons={this.state.persons}
+          personsLength={this.state.persons.length}
           clicked={this.togglePersonsHandler} />
           ): null}
         {persons}
