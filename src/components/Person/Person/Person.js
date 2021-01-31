b87a7b7fc5fd177744c8b42aae2a12151f3e9dcf
Touhid7051 <touhidul15-7051@diu.edu.bd>
2021-01-31 19:38:28 +0600

    Rendaring Adjacent JSX(Auxiliary)

--- src/components/Person/Person/Person.js
+++ src/components/Person/Person/Person.js
@@ -1,27 +1,21 @@
-import React,{ Component } from 'react';
+import React from 'react';
 import classes from './Person.css';
+import Aux from '../../../hoc/Auxiliary';
 
 
 
 
-class Person extends Component {
-    render(){
+const person = (props) => {
     console.log('[Person.js] rendering');
-    return[
-        <div className={classes.Person} >
-           
-                <p onClick={this.props.click}>
-                I am {this.props.name} and I am {this.props.age} years old!  
-                </p>,
-                <p>{this.props.children}</p>,
-                <input type='text' 
-                onChange={this.props.changed} 
-                value={this.props.name} />
-                </div>
-    ];
-    }
-    
+    return (
+        <Aux>
+
+            <p onClick={props.click}>I am {props.name} and I am {props.age} years old!  </p>
+            <p>{props.children}</p>
+            <input type='text' onChange={props.changed} value={props.name} />
+        </Aux>
+    )
 
 };
 
-export default Person;
+export default person;
