6034b6a3037f3c17e1bc60cf9f15d1c5e8710b72
Touhid7051 <touhidul15-7051@diu.edu.bd>
2021-01-31 19:08:22 +0600

    Adjacent JSX

--- src/components/Person/Person/Person.js
+++ src/components/Person/Person/Person.js
@@ -7,14 +7,18 @@ import classes from './Person.css';
 class Person extends Component {
     render(){
     console.log('[Person.js] rendering');
-    return(
+    return[
         <div className={classes.Person} >
            
-                <p onClick={this.props.click}>I am {this.props.name} and I am {this.props.age} years old!  </p>
-                <p>{this.props.children}</p>
-                <input type='text' onChange={this.props.changed} value={this.props.name} />
+                <p onClick={this.props.click}>
+                I am {this.props.name} and I am {this.props.age} years old!  
+                </p>,
+                <p>{this.props.children}</p>,
+                <input type='text' 
+                onChange={this.props.changed} 
+                value={this.props.name} />
                 </div>
-         );
+    ];
     }
     
 
